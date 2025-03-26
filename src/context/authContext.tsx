b5246fbf6aeeefc411a7126831a1294ea3ddc681
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-03-26 11:28:58 -0300

    feat: criacao do contexto de autorizacao

---
+++ src/context/authContext.tsx
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface AuthState {
+    token: string | null;
+    isAuthenticated: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+    token: null,
+    isAuthenticated: false,
+    login: (token) =>
+        set({ token, isAuthenticated: true }),
+    logout: () =>
+        set({ token: null, isAuthenticated: false }),
+}));
