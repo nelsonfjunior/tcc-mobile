6c146ada022a56a5fe8ab6878f445a6ab5036905
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-03-28 16:12:52 -0300

    api de acesso

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL = "http://10.0.2.2:8080"; //localhost no emulador android
+//export const API_BASE_URL = "http://192.168.1.103:8080"; //meu ip
 
 const api = axios.create({
     baseURL: API_BASE_URL,
