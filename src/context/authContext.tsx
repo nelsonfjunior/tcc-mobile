4984f9bbb678d4c6a031a269b12d550d7d44a173
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-04-14 20:59:05 -0300

    refact: salvar o usuario no contexto

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -2,31 +2,36 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
 
 interface AuthState {
-    token: string | null;
+    token: string;
+    userId: string;
     isAuthenticated: boolean;
-    login: (token: string) => Promise<void>;
+    login: (token: string, userId: string) => Promise<void>;
     logout: () => Promise<void>;
     loadToken: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    token: null,
+    token: '',
+    userId: '',
     isAuthenticated: false,
 
-    login: async(token) =>{
+    login: async(token, userId) =>{
         await AsyncStorage.setItem("authToken", token);
-        set({ token, isAuthenticated: true });
+        await AsyncStorage.setItem("userId", userId);
+        set({ token, userId, isAuthenticated: true });
     },
 
     logout: async() => {
         await AsyncStorage.removeItem("authToken");
-        set({ token: null, isAuthenticated: false });
+        await AsyncStorage.removeItem("userId");
+        set({ token: '', userId: '', isAuthenticated: false });
     },
 
     loadToken: async () => {
         const storedToken = await AsyncStorage.getItem("authToken");
-        if (storedToken) {
-            set({ token: storedToken, isAuthenticated: true });
+        const storedUserId = await AsyncStorage.getItem("userId");
+        if (storedToken && storedUserId) {
+            set({ token: storedToken, userId: storedUserId, isAuthenticated: true });
         }
     },
 
